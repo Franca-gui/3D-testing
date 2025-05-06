b9790e4ea7a826518b0d5d3191b524c3bc15a63f
França-gui <guilherme_j_franca@estudante.sesisenai.org.br>
2025-05-06 10:58:02 -0300

    add animation

--- main.js
+++ main.js
@@ -17,3 +17,13 @@ const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
 const cube = new THREE.Mesh( geometry, material ); 
 scene.add( cube );
+
+camera.position.z = 5
+
+function animate(){
+requestAnimationFrame(animate);
+
+renderer.render(scene, camera);
+}
+
+animate();
