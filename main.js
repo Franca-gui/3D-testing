646442b4fa20df76c31b432a311160f681f7cd22
França-gui <guilherme_j_franca@estudante.sesisenai.org.br>
2025-05-06 11:47:19 -0300

    JUST TRYING TO MAKE IT WORK

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import * as THREE from 'three';
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.camera(
@@ -13,7 +15,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
+const geometry = new THREE.BoxGeometry( 2, 2, 2 ); 
 const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
 const cube = new THREE.Mesh( geometry, material ); 
 scene.add( cube );
@@ -22,7 +24,7 @@ camera.position.z = 5
 
 function animate(){
 requestAnimationFrame(animate);
-
+cube.rotation.x += 0.01;
 renderer.render(scene, camera);
 }
 
